32db5db8d03f069b4f0b7669c51773711c9dcc55
themaleem <themaleem@gmail.com>
2020-03-29 19:04:59 +0100

    polishing project

--- lol/urls.py
+++ lol/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path,include
+from todo.views import login_view,logout_view,register_view
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('urlshortner.urls',namespace='urlshortner')),
-
-    path('', include('todo.urls',namespace='notes')),
-
+    path('notes', include('todo.urls',namespace='notes')),
+    path('accounts/login/', login_view, name='login'),
+    path('accounts/logout/', logout_view, name='logout'),
+    path('accounts/register/', register_view, name='register'),
 ]
--- todo/urls.py
+++ todo/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path,include
 
 from .views import home, login_view, register_view, logout_view, note_detail, edit_note, delete_note, LandingView
 
@@ -7,13 +7,10 @@ app_name = 'notes'
 urlpatterns = [
 
     #home page and dashboard
-    path('', LandingView.as_view() , name='landing' ),
-    path('notes/mine/', home, name='home' ),
-    path('accounts/login/', login_view, name='login'),
-    path('accounts/logout/', logout_view, name='logout'),
-    path('accounts/register/', register_view, name='register'),
+    path('home', LandingView.as_view() , name='landing' ),
+    path('mine/', home, name='home' ),
     path('<slug:slug>/<int:pk>/', note_detail, name='note_detail'),
-    path('edit/note/<slug:slug>/<int:pk>', edit_note, name='edit_note'),
-    path('delete/note/<slug:slug>/<int:pk>', delete_note, name='note_confirm_delete'),
+    path('<slug:slug>/<int:pk>/edit', edit_note, name='edit_note'),
+    path('<slug:slug>/<int:pk>/delete', delete_note, name='note_confirm_delete'),
 
 ]
